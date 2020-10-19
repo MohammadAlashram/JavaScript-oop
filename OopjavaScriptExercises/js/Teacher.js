359c062a2bf2bdcecf6d6a52fff0d8b49fb6a3bf
MohammadAlashram <71894852+MohammadAlashram@users.noreply.github.com>
2020-10-19 14:46:56 +0300

    Oop javaScript Exercises

--- OopjavaScriptExercises/js/Teacher.js
+++ OopjavaScriptExercises/js/Teacher.js
@@ -1,9 +1,9 @@
-    class Teacher extends Student{
+class Teacher extends Student{
         constructor(Salary ,subjects,Id,name,Email,mobile_number){
             super(Id,name,Email,mobile_number);
             this.Salary = Salary;
             this.subjects = subjects;
         }
     }
-let teacher1 = new Teacher(800,'[English, Arabic, Math, science]',123443,'Sara','sara@orange.com',0777777777);
+let teacher1 = new Teacher(800,'[English, Arabic, Math, science]',96432,'Sadi','Sadi@orange.com',0777788888);
 console.log(teacher1);
